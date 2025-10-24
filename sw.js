910d2717e6cee1c4b3e41f2bc162fa4d832c9c58
sk8erboi577-hash <sk8erboi577@gmail.com>
2025-10-24 23:35:31 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-// 不攔截導覽，先完全當作沒 SW：讓瀏覽器照常請求 /CCpaka/ 路徑
